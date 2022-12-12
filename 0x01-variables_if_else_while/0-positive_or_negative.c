6be912554530bc73eeb5b7aa81a08520904c26af
Your Name <you@example.com>
2022-12-12 09:35:24 -0800

    fv

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,32 @@
-The number, followed by
-	if the number is greater than 0: is positive
-	if the number is 0: is zero
-	if the number is less than 0: is negative
-followed by a new line
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function to generate a random number
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	}
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf(%d is negative\n", n);
+	}
+	return (0);
+}

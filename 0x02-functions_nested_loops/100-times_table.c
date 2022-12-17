b0472acda00cc36eff79f544443b8e414f6311b8
Your Name <you@example.com>
2022-12-17 08:33:14 -0800

    fr

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times table
+ * @n: number to end at
+ *
+ * Return: Always void
+ */
+
+void print_times_table(int n)
+{
+	int a = 0, row_num = n + 1, col_num = n + 1;
+	int adder = 0;
+
+	if (n > 15 || n < 0)
+		return;
+
+	while (row_num > 0)
+	{
+		while (col_num > 0)
+		{
+			printnum(a);
+			a += adder;
+			if (col_num != 1)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (a < 10)
+					_putchar(' ');
+				if (a < 100)
+					_putchar(' ');
+			}
+			col_num--;
+		}
+		a = 0;
+		adder++;
+		_putchar('\n');
+		col_num = n + 1;
+		row_num--;
+	}
+}
+
+/**
+ * printnum - prints num
+ * @v: num it prints
+ *
+ * Return: Always void
+ */
+
+void printnum(int v)
+{
+	if (v < 0)
+	{
+		_putchar('-');
+		v = -v;
+	}
+
+	if (v < 10)
+		_putchar(v + '0');
+	else if (v >= 10 && v < 100)
+	{
+
+		_putchar((v / 10) + '0');
+		_putchar((v % 10) + '0');
+	}
+	else if (v >= 100 && v < 1000)
+	{
+		_putchar((v / 100) + '0');
+		_putchar(((v - ((v / 100) * 100)) / 10) + '0');
+		_putchar((v % 10) + '0');
+	}
+}
+

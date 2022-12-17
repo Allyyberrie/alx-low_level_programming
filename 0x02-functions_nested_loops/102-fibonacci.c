d5d4fcce41132b4c8e0a5394a2559be954de2986
Your Name <you@example.com>
2022-12-17 09:18:21 -0800

    fr

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: "The program prints first 50 fibonnaci nums"
+ * Return; Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 1;
+	b = 2;
+
+	printf("%lu, %lu", a, b);
+
+	for (i = 0, i < 48; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b =c;
+	}
+	putchar('\n');
+
+	return (0);
+}

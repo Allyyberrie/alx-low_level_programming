353de4bc28aef1752354936b964aa8856d555c1b
Your Name <you@example.com>
2022-12-19 06:20:30 -0800

    fr

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'get the largest prime factor'
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	long int num, div maxFact;
+
+	num = 612852475143;
+
+	div = 2;
+
+	while (num != 0)
+	{
+		if (num % div != 0)
+		{
+			div++;
+		}
+		else
+		{
+			maxFact = num;
+			num = num / div;
+			if (num == 1)
+			{
+				printf("%lu", maxFact);
+				break;
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}

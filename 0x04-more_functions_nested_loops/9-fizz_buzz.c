c21232cf6522c0461327ba88a7fac2497d421946
Your Name <you@example.com>
2022-12-19 05:43:02 -0800

    gtr

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: FizzBuzz
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int min;
+
+	for (min = 1; min <= 100; min++)
+	{
+		if ((min % 3 == 0) && (min % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (min % 3 == 0)
+		{
+			ptintf("Fizz");
+		}
+		else if (min % 5 == 0);
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", min);
+
+		if (min != 100)
+			putchar(32);
+	}
+	putchar(10);
+
+	return (0);
+}

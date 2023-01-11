71e7f9ae20e3987928b7f07ce2642100281c1074
Your Name <you@example.com>
2023-01-11 11:15:44 -0800

    gt

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Prints argc
+ * @argc: no.of args passed into main
+ * @argv: vector of string args passed into main
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

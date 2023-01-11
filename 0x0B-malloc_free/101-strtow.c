d6d5f7cb49fa0310195ba9a9a0fe5d5c92e7d574
Your Name <you@example.com>
2023-01-10 23:13:14 -0800

    gt

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits strings to words
+ * @str: string to split
+ *
+ * Return: char **
+ */
+
+char **strtow(char *str)
+{
+	int i, arr_len = 0;
+	char **value;
+
+	if (str == NULL)
+		return (NULL);
+
+
+	arr_len = 1;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			arr_len++;
+	}
+
+	value = (char **)malloc(sizeof(char *) * arr_len);
+
+	return (value);
+}

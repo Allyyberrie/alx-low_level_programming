17b97c9bcb1eadb7888a9a1d1185beb1869c54fb
Your Name <you@example.com>
2023-01-12 11:28:44 -0800

    git

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: 'A program that prints "_putchar"'
+ *
+ * Return: 0(Success)
  */
+
 int main(void)
 {
-	char *sh = "main";
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 't';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+	char i = '/n';
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar(i);
 
 	return (0);
 }
